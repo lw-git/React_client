776334677e2d4257ed557db7a0a6e20ff0103f5c
lw <lw_git@mail.ru>
2020-06-17 09:31:07 +0500

    Added bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+  }
+
+  componentDidMount(){
+
+  }
+
+  render() {
+    return (
+      <div className={'container'}>
+        <div className={'row'}>
+          <div className={'col-lg-8 mx-auto'}>
+            <div className={'card'}>
+              <div className={'card-header bg-primary text-white'}>
+                <h1>Todo:</h1>
+              </div>
+              <div className={'card-body'}>
+                Completed:&nbsp;
+                <input type={"checkbox"}
+                />
+                <div className={"add-items d-flex mb-4"}>
+                  <input className={"form-control"}
+                         type={"text"}
+                  />
+                  <button className={"btn btn-primary"}>Create
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
